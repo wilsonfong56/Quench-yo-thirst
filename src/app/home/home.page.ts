579cda3f125d46664f5be605d190711597cc8d35
wilsonfong56 <63813902+wilsonfong56@users.noreply.github.com>
2023-02-20 02:13:15 -0800

    Added Geolocation to home.page.ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,13 @@
 import { Component } from '@angular/core';
 
 import { HealthKit } from '@awesome-cordova-plugins/health-kit/ngx';
+import { Geolocation } from '@capacitor/geolocation';
+
+const printCurrentPosition = async () => {
+  const coordinates = await Geolocation.getCurrentPosition();
+
+  console.log('Current position:', coordinates);
+};
 
 @Component({
   selector: 'app-home',
