d48183e0570c6e9b6ee50e8c2d6722248ab6da40
wilsonfong56 <63813902+wilsonfong56@users.noreply.github.com>
2023-02-20 14:29:56 -0800

    added Geolocation

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,19 +3,26 @@ import { Component } from '@angular/core';
 import { HealthKit } from '@awesome-cordova-plugins/health-kit/ngx';
 import { Geolocation } from '@capacitor/geolocation';
 
-const printCurrentPosition = async () => {
-  const coordinates = await Geolocation.getCurrentPosition();
-
-  console.log('Current position:', coordinates);
-};
-
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
 
-  constructor() {}
+export class HomePage {
+  latitude = 0;
+  longitude = 0;
+  height = "";
+  weight = "";
+
+  constructor() {
+    this.getCurrentPosition();
+  }
+
+  async getCurrentPosition() {
+    const position = await Geolocation.getCurrentPosition();
+    this.latitude = position.coords.latitude;
+    this.longitude = position.coords.longitude;
+  }
 
 }
