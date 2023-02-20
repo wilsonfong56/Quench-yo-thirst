1e74baa33270f459084dbb2613df395679911215
wilsonfong56 <63813902+wilsonfong56@users.noreply.github.com>
2023-02-20 14:49:09 -0800

    Test

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 import { HealthKit } from '@awesome-cordova-plugins/health-kit/ngx';
 import { Geolocation } from '@capacitor/geolocation';
 
@@ -14,9 +14,11 @@ export class HomePage {
   longitude = 0;
   height = "";
   weight = "";
+  //weather: any;
 
   constructor() {
     this.getCurrentPosition();
+    // this.getWeather();
   }
 
   async getCurrentPosition() {
@@ -24,5 +26,13 @@ export class HomePage {
     this.latitude = position.coords.latitude;
     this.longitude = position.coords.longitude;
   }
+
+  // getWeather() {
+  //   // Change this later so api key is hidden
+  //   const url = 'https://api.openweathermap.org/data/2.5/weather?lat=44&lon=-134}&appid=fa845a8dfaa2d70c613ea149690dc198'; 
+  //   this.http.get(url).subscribe((data: any) => {
+  //     this.weather = data;
+  //   });
+  //}
 
 }
