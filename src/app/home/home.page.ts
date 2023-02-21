a6a8c1a7e5a1ab6fbc61b288255cc94eae8b74f0
Brennen Wong <brennen.wong@gmail.com>
2023-02-21 01:11:20 -0800

    added HealthKit developer functionality

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,8 @@
 import { Component, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { HealthKit } from '@awesome-cordova-plugins/health-kit/ngx';
+import { HealthKit, HealthKitOptions} from '@awesome-cordova-plugins/health-kit/ngx';
 import { Geolocation } from '@capacitor/geolocation';
+import { Platform } from '@ionic/angular';
 
 
 @Component({
@@ -16,13 +17,49 @@ export class HomePage {
   height = "";
   weight = "";
   temp = 0;
+  stepcount = "No Data";
 
   constructor(
     private http: HttpClient,
     private healthKit: HealthKit,
-    @Inject('API_KEY') private apiKey: string) {
+    @Inject('API_KEY') private apiKey: string,
+	private plt: Platform) {
+
     console.log(this.apiKey)
     this.ngOnInit();
+
+	this.plt.ready().then(() => {
+		this.healthKit.available().then(available => {
+		  if (available) {
+			// Request all permissions up front if you like to
+			var options: HealthKitOptions = {
+			  readTypes: ['HKQuantityTypeIdentifierHeight', 'HKQuantityTypeIdentifierStepCount', 'HKWorkoutTypeIdentifier', 'HKQuantityTypeIdentifierActiveEnergyBurned', 'HKQuantityTypeIdentifierDistanceCycling'],
+			  writeTypes: ['HKQuantityTypeIdentifierHeight', 'HKWorkoutTypeIdentifier', 'HKQuantityTypeIdentifierActiveEnergyBurned', 'HKQuantityTypeIdentifierDistanceCycling']
+			}
+			this.healthKit.requestAuthorization(options).then(_ => {
+			  this.loadHealthData();
+			})
+		  }
+		});
+	  });
+  }
+
+
+  async loadHealthData() {
+    var stepOptions = {
+      startDate: new Date(new Date().getTime() - 24 * 60 * 60 * 1000),
+      endDate: new Date(),
+      sampleType: 'HKQuantityTypeIdentifierStepCount',
+      unit: 'count'
+    }
+
+    this.healthKit.querySampleType(stepOptions).then(data => {
+      let stepSum = data.reduce((a: any, b: any) => a + b.quantity, 0);
+      this.stepcount = stepSum;
+    }, err => {
+      console.log('No steps: ', err);
+    });
+
   }
 
   async ngOnInit() {
