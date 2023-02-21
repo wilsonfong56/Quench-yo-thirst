aa4e87ebfaabd0c1ca85a9af239ccbda2b81f839
wilsonfong56 <63813902+wilsonfong56@users.noreply.github.com>
2023-02-21 15:58:54 -0800

    Added openWeather functionality

---
+++ Quench-yo-thirst-main/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  apiKey: 'fa845a8dfaa2d70c613ea149690dc198',
+  firebase: {
+    apiKey: "AIzaSyBVD67YFUBLEmE1xiRpW4dMhjRc38BJ24U",
+    authDomain: "quench-yo-thirst-uci.firebaseapp.com",
+    projectId: "quench-yo-thirst-uci",
+    storageBucket: "quench-yo-thirst-uci.appspot.com",
+    messagingSenderId: "588521470031",
+    appId: "1:588521470031:web:99bd430be55de87b11123a",
+    measurementId: "G-5N67CQPBT4"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
