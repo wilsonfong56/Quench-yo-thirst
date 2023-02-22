2ec45d8b32b5902d8c3452dae95c3aa438b9de8a
Wilson Fong <63813902+wilsonfong56@users.noreply.github.com>
2023-02-21 16:18:50 -0800

    added openWeather api key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
+  apiKey: 'fa845a8dfaa2d70c613ea149690dc198',
   firebase: {
     projectId: 'quench-yo-thirst-uci',
     appId: '1:588521470031:web:99bd430be55de87b11123a',
