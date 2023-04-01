cdec28f5e23009e68e4b1bdcb242ee27566e45a4
Wilson Fong <63813902+wilsonfong56@users.noreply.github.com>
2023-04-01 02:37:44 -0700

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
-  apiKey: 'fa845a8dfaa2d70c613ea149690dc198',
+  apiKey: 'YOUR_API_KEY',
   firebase: {
     projectId: 'quench-yo-thirst-uci',
     appId: '1:588521470031:web:99bd430be55de87b11123a',
     storageBucket: 'quench-yo-thirst-uci.appspot.com',
-    apiKey: 'AIzaSyBVD67YFUBLEmE1xiRpW4dMhjRc38BJ24U',
+    apiKey: 'YOUR_API_KEY',
     authDomain: 'quench-yo-thirst-uci.firebaseapp.com',
     messagingSenderId: '588521470031',
     measurementId: 'G-5N67CQPBT4',
